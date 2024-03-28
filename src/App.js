aa89221a801cc833cbb4c854c9886e18a23d6e2f
shashi-avyay <shashikant.kumar@avyay.solutions>
2024-03-28 20:18:59 +0530

    Update Learn React link

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from avyay
+          Learn React from Archlynk
         </a>
       </header>
     </div>
